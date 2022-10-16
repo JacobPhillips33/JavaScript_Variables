e51feceb2c7b387902bba5290fa3af95fe7f92b3
Jacob Phillips <jakephillips.wv@gmail.com>
2022-10-16 14:02:03 -0500

    completed exercises

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 and Exercise 2
+const firstName = "Jacob";
+let lastName = "Phillips";
+var age = 33;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+// Writing to Console Practice
+console.log(title);
+console.log(hasRead);
+console.log(firstName);
+console.log(salePercentage);
